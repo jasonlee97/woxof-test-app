4850d3d58e3607d4393e9d3949858aa227c9b174
Jason Lee <jasonlee1054@gmail.com>
2018-09-19 18:32:41 -0700

    User profile UI done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,12 @@
 <div class="home-body">
     <form method="POST" action="{{ route('homeUpdate') }}">
         @csrf
-        <label>Name</label>
-        <input type="text" name="name" value="{{ old('name', Auth::user()->name) }}">
-        <label>Surname</label>
-        <input type="text" name="surname" value="{{ old('surname', Auth::user()->surname) }}">
-        <label>Email</label>
-        <input type="email" name="email" value="{{ old('email', Auth::user()->email) }}">
+        <label>Name</label><br>
+        <input type="text" name="name" value="{{ old('name', Auth::user()->name) }}"><br>
+        <label>Surname</label><br>
+        <input type="text" name="surname" value="{{ old('surname', Auth::user()->surname) }}"><br>
+        <label>Email</label><br>
+        <input type="email" name="email" value="{{ old('email', Auth::user()->email) }}"><br>
         <button type="submit">Submit</button>
     </form>
 </div>
